955564af7d6bb4d1097c501d6d2ff8d94af2a2b0
TankEngine-ish <131184681+TankEngine-ish@users.noreply.github.com>
2024-02-16 19:39:34 +0200

    create desktop icon

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Use Zenity to create a file selection dialog and get the selected file path
+# creates a file selection dialog to get the selected file path
 file_path=$(zenity --file-selection --file-filter='*.webp' --title="Select a .webp image")
 
-# Use Zenity to create a list dialog and get the selected output format
+# creates a list dialog to get the selected output format
 output_format=$(zenity --list --radiolist --title="Select output format" --column="Select" --column="Format" TRUE "jpeg" FALSE "png")
 
-# Check if the selected file is a .webp image
+# Checks if the selected file is a .webp image
 if [[ $file_path == *.webp ]]; then
     if [[ $output_format == "jpeg" ]]; then
         # Convert the .webp image to .ppm format, then to .jpeg format, and delete the .ppm file
@@ -15,9 +15,9 @@ if [[ $file_path == *.webp ]]; then
         # Convert the .webp image to .png format
         dwebp "$file_path" -o "${file_path%.webp}.png"
     fi
-    # Use Zenity to show a success message
+   
     zenity --info --text="Image converted successfully!"
 else
-    # Use Zenity to show an error message
+   
     zenity --error --text="Selected file is not a .webp image!"
 fi
