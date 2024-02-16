ce071afc5019722d7167b166f7620cd29dfb59ea
TankEngine-ish <131184681+TankEngine-ish@users.noreply.github.com>
2024-02-16 19:17:21 +0200

    convert to jpeg - success

--- script.sh
+++ script.sh
@@ -10,7 +10,7 @@ output_format=$(zenity --list --radiolist --title="Select output format" --colum
 if [[ $file_path == *.webp ]]; then
     if [[ $output_format == "jpeg" ]]; then
         # Convert the .webp image to .ppm format, then to .jpeg format, and delete the .ppm file
-        dwebp "$file_path" -o "${file_path%.webp}.ppm" && cjpeg "${file_path%.webp}.ppm" > "${file_path%.webp}.jpg" && rm "${file_path%.webp}.ppm"
+        dwebp "$file_path" -ppm -o "${file_path%.webp}.ppm" && cjpeg "${file_path%.webp}.ppm" > "${file_path%.webp}.jpg" && rm "${file_path%.webp}.ppm"
     elif [[ $output_format == "png" ]]; then
         # Convert the .webp image to .png format
         dwebp "$file_path" -o "${file_path%.webp}.png"
