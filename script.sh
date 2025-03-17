76631bda6f68c001c739c587d2bc307aac5a7c43
Plamen Dimitrov <plamendimitrov0308@yahoo.com>
2025-03-17 23:42:57 +0200

    add .ppm description

--- script.sh
+++ script.sh
@@ -9,17 +9,14 @@ if [ $? -ne 0 ]; then
     exit
 fi
 
-
-# creates a list dialog to get the selected output format
 output_format=$(zenity --list --radiolist --title="Select output format" --column="Select" --column="Format" TRUE "jpeg" FALSE "png")
 
-# Checks if the selected file is a .webp image
 if [[ $file_path == *.webp ]]; then
     if [[ $output_format == "jpeg" ]]; then
-        # Convert the .webp image to .ppm format, then to .jpeg format, and delete the .ppm file
+        # we convert the .webp image to .ppm format, then to .jpeg format, and afterwards we delete the .ppm file. The reason for all of that is cjpeg doesn't support webp directly afaik.
         dwebp "$file_path" -ppm -o "${file_path%.webp}.ppm" && cjpeg "${file_path%.webp}.ppm" > "${file_path%.webp}.jpg" && rm "${file_path%.webp}.ppm"
     elif [[ $output_format == "png" ]]; then
-        # Convert the .webp image to .png format
+        # straight to .png
         dwebp "$file_path" -o "${file_path%.webp}.png"
     fi
    
